ad77d0016597d3f47dc91ce82f7085064f30bf47
Abdul Wasi Bhat <bhatabdul75@gmail.com>
2025-03-06 19:29:20 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let timerDisplay = document.getElementById("timer");
+    let startBtn = document.getElementById("startBtn");
+    let resetBtn = document.getElementById("resetBtn");
+
+    startBtn.addEventListener("click", () => {
+        alert("Timer Started (Logic will be added)");
+    });
+
+    resetBtn.addEventListener("click", () => {
+        alert("Timer Reset (Logic will be added)");
+    });
+});
